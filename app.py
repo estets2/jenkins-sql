e91dd38848edbc1523255ecc85edf7fbc60247b3
estets2 <estets@mail.ru>
2021-02-17 19:40:29 +0300

    add environ

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import os
 import psycopg2
 from psycopg2 import Error
 
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASS = os.environ.get('POSTGRES_PASS')
+
 try:
     # Connect to an existing database
-    connection = psycopg2.connect(user="postgres",
-                                  password="pynative@#29",
-                                  host="127.0.0.1",
+    connection = psycopg2.connect(user=POSTGRES_USER,
+                                  password=POSTGRES_PASS,
+                                  host=POSTGRES_HOST,
                                   port="5432",
-                                  database="postgres_db")
+                                  database="docker")
 
     # Create a cursor to perform database operations
     cursor = connection.cursor()
